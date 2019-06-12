bc408772865ed500484ed0c9fbd0d98fb69354bf
Bukhteev <bukhteev.kirill@gmail.com>
2019-06-13 01:42:04 +0300

    add 6 hw

--- 06/sort.cpp
+++ 06/sort.cpp
@@ -7,14 +7,13 @@
 #include <iterator>
 #include <thread>
 #include <experimental/filesystem>
-#include <future>
+#include <condition_variable>
+
 
 namespace fs = std::experimental::filesystem;
 
 std::mutex mutex;
-size_t batch_size = 400000;      
-//столько uint64_t вмещается в 4mb,так как у нас два потока, то нужно распределить для каждого поровну
-//Я сделал на 10000 меньше, потому что некоторые операции могут потребовать дополнительную память при обработке
+size_t batch_size = 10000;      
 
  //Будем сортировать методом слияния файлы. Воспользуемся методом merge из stl 
 void merge_file(const std::string f1, const std::string f2, const std::string f_out ){
@@ -29,61 +28,62 @@ void merge_file(const std::string f1, const std::string f2, const std::string f_
    return;
 }
 
-void my_sort(const char* file, const std::string& prefix) {
+void my_sort(const char* file, const std::string& prefix, size_t num) {
     std::vector<uint64_t> vec(batch_size);
-    //Исользование метода из СИ, на сколько я разузнал в интернете, более эффективно для блочного считывание, чем считывание с помощью ifstream итеративно
+    
     FILE *fp;
     if ((fp = fopen(file, "rb")) == NULL) {
         std::cout << "Cannot open file." << std::endl;
         return;
     }
-    
 
     int last = 0;
+    int start = 0;
+    if (num == 1 ){
+        last = 1;
+        start =1;
+        }
+        
 //считываем файл по по кусочкам, чуть меньше 8 mb, сортируем их и сразу же записываем в файл для дальнейшей сортировки слиянием
     while(!feof(fp)) {
         
-        std::lock_guard<std::mutex> lock(mutex);
+        fseek(fp,  sizeof(uint64_t) * batch_size * last, SEEK_SET);
         fread(vec.data(), sizeof(uint64_t), batch_size, fp);
-        
-
         std::sort(begin(vec), end(vec));
         std::ofstream out("data/" + prefix + std::to_string(last) + ".bin",
                         std::ios::binary);
-        for (auto j : vec) { //записываем данные в файл, пока не дойдем до последнего символа
+        for (auto j : vec) { 
             if (j != 0)
                 out << j << " ";
             else
                 continue;
         }
-        last++;
+
+        last += 2;
     }
     fclose(fp);
 
 
- size_t start = 0;
+
 
     std::string file_1 = "data/" + prefix + std::to_string(start) + ".bin";
     std::string file_2;
     std::string file_3;
-    while (start < last-1){
+    while (start < last - 1){
         
-
-        file_2 = "data/" + prefix + std::to_string(start + 1) + ".bin";
-        file_3 = "data/new" + prefix + std::to_string(start + 1) + ".bin";
-    
+        file_2 = "data/" + prefix + std::to_string(start + 2) + ".bin";
+        file_3 = "data/new" + prefix + std::to_string(start + 2) + ".bin";   
         merge_file(file_1, file_2, file_3);
 
-        ++start;
-        
+        start += 2;
         file_1 = "data/new" + prefix + std::to_string(start) + ".bin";
-        if (start > 1){
-            fs::remove("data/new" + prefix + std::to_string(start - 1) + ".bin");
+
+        if (start > 3){
+            fs::remove("data/new" + prefix + std::to_string(start - 2) + ".bin");
 
         }
     }
-    std::rename((file_1).c_str(),
-                ("data/" + prefix + "output.bin").c_str() );
+    std::rename((file_1).c_str(), ("data/" + prefix + "output.bin").c_str() );
     return;
 }
 
@@ -96,8 +96,8 @@ int main(int argc, char* argv[]) {
     fs::create_directories("data");
     auto file = argv[1];
     auto outputFile = argv[2];
-    std::thread t1(my_sort, file, "one_");
-    std::thread t2(my_sort, file, "two_");
+    std::thread t1(my_sort, argv[1], "one_", 1);
+    std::thread t2(my_sort, file, "two_", 2);
 
     t1.join();
     t2.join();
