d423ac273770e45e7bb7f93807f5642858a1b1bf
Bukhteev <bukhteev.kirill@gmail.com>
2019-06-12 14:52:18 +0300

    add 06 hw

--- 06/make_file.cpp
+++ 06/make_file.cpp
@@ -5,7 +5,7 @@
 int main() {
     srand((unsigned)time(NULL));
     FILE* f = fopen("in2.bin", "w");
-    int n = 200000000 + rand()%25;
+    int n = 20000000 + rand()%25;
     for (int i = 0; i < n; i++) {
         int a = 0 + rand()%10;
         fwrite(&a, sizeof(int), 1, f);
--- 06/sort.cpp
+++ 06/sort.cpp
@@ -12,10 +12,12 @@
 namespace fs = std::experimental::filesystem;
 
 std::mutex mutex;
-size_t batch_size = 500000;      //столько uint64_t вмещается в 8mb
+size_t batch_size = 400000;      
+//столько uint64_t вмещается в 4mb,так как у нас два потока, то нужно распределить для каждого поровну
+//Я сделал на 10000 меньше, потому что некоторые операции могут потребовать дополнительную память при обработке
 
  //Будем сортировать методом слияния файлы. Воспользуемся методом merge из stl 
-void merge_file(const std::string& f1, const std::string& f2, const std::string& f_out ){
+void merge_file(const std::string f1, const std::string f2, const std::string f_out ){
     std::ifstream f1_op(f1, std::ios::binary );
     std::ifstream f2_op(f2, std::ios::binary );
     std::ofstream mergeFile(f_out, std::ios::binary );
@@ -29,6 +31,7 @@ void merge_file(const std::string& f1, const std::string& f2, const std::string&
 
 void my_sort(const char* file, const std::string& prefix) {
     std::vector<uint64_t> vec(batch_size);
+    //Исользование метода из СИ, на сколько я разузнал в интернете, более эффективно для блочного считывание, чем считывание с помощью ifstream итеративно
     FILE *fp;
     if ((fp = fopen(file, "rb")) == NULL) {
         std::cout << "Cannot open file." << std::endl;
@@ -66,31 +69,39 @@ void my_sort(const char* file, const std::string& prefix) {
     while (start < last-1){
         
 
-        file_2 = "data/" + prefix + std::to_string(start+1) + ".bin";
-        file_3 = "data/new" + prefix + std::to_string(start+1) + ".bin";
+        file_2 = "data/" + prefix + std::to_string(start + 1) + ".bin";
+        file_3 = "data/new" + prefix + std::to_string(start + 1) + ".bin";
     
         merge_file(file_1, file_2, file_3);
 
         ++start;
         
         file_1 = "data/new" + prefix + std::to_string(start) + ".bin";
-        
+        if (start > 1){
+            fs::remove("data/new" + prefix + std::to_string(start - 1) + ".bin");
+
+        }
     }
     std::rename((file_1).c_str(),
                 ("data/" + prefix + "output.bin").c_str() );
     return;
 }
 
-int main() {
-    std::string file = "in2.bin";
+int main(int argc, char* argv[]) {
+    if (argc != 3 ) {
+        std::cout << "Please, enter names of input and output files" << std::endl;
+        return 0;
+    }
+
     fs::create_directories("data");
-    std::thread t1(my_sort, file, "first_");
-    std::thread t2(my_sort, file, "second_");
+    auto file = argv[1];
+    auto outputFile = argv[2];
+    std::thread t1(my_sort, file, "one_");
+    std::thread t2(my_sort, file, "two_");
 
     t1.join();
     t2.join();
-
-    merge_file("data/first_output.bin", "data/second_output.bin", "sorted_file.bin");
+    merge_file("data/one_output.bin", "data/two_output.bin", outputFile);
 
     fs::remove_all("data");
     return 0;
